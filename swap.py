e43f0888b937250b28f752c5f8a5d37850a6397f
Charith Jeewantha <ucjeewantha@gmail.com>
2022-10-10 16:52:03 +0530

    Swap Two Numbers By Python.

---
+++ swap.py
@@ -0,0 +1,10 @@
+num_1 = int(input("Enter your first number: "))
+num_2 = int(input("Enter your second number: "))
+
+print("Before swapping number1 =", num_1, ", number2 =", num_2)
+
+temp = num_1
+num_1 = num_2
+num_2 = temp
+
+print("After swapping number1 =", num_1, ", number2 =", num_2)
